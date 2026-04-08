dcbcf2f51457d4d5eee51ef28d19aab32bb2a705
Andrea Lerose <a.lerose@itispa.com>
2026-04-08 11:45:19 +0200

    fine esercizio_con ritocchi finali

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 let corrente = 2026;
 let nascita = 1986;
 let difference = corrente - nascita;
-console.log(difference);
+console.log(' Io quest anno compirò ' + (difference) + ' anni ');
 
 let anni = 40;
 let cento = 100;
 let mancanti = cento - anni;
-console.log(mancanti);
+console.log(' Mi mancano ' + (mancanti) + ' anni per arrivare ai 100 ');
 
-console.log('Hai ' + (difference) + ' anni e mancano ' + (mancanti) + ' anni per arrivare a 100 ');
+console.log('Ho ' + (difference) + ' anni e mi mancano ' + (mancanti) + ' anni per arrivare a 100 ');
