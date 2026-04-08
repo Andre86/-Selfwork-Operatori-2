b71658ddcd4e0e798fa2d25ee9b609fd9377897b
Andrea Lerose <a.lerose@itispa.com>
2026-04-08 11:32:15 +0200

    esercizio anni - 1

---
+++ main.js
@@ -0,0 +1,10 @@
+// Quanti anni compirò nel 2026?
+let corrente = 2026;
+let nascita = 1986;
+let difference = corrente - nascita;
+console.log(difference);
+
+let anni = 40;
+let cento = 100;
+let mancanti = cento - anni;
+console.log(mancanti)
