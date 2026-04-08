7d0ed036753bc0858de1814cc81e0e073e79dcb7
Andrea Lerose <a.lerose@itispa.com>
2026-04-08 11:41:30 +0200

    fine esercizio

--- main.js
+++ main.js
@@ -7,4 +7,6 @@ console.log(difference);
 let anni = 40;
 let cento = 100;
 let mancanti = cento - anni;
-console.log(mancanti)
+console.log(mancanti);
+
+console.log('Hai ' + (difference) + ' anni e mancano ' + (mancanti) + ' anni per arrivare a 100 ');
